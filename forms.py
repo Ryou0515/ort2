f50feee56aa4dd2bb9b8460150c63dfcfdddd6fd
Ryou0515 <167046748+Ryou0515@users.noreply.github.com>
2024-12-02 11:23:05 +0900

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Product
+
+# 検索フォーム
+class SearchForm(forms.Form):
+    query = forms.CharField(
+        label='検索キーワード',
+        max_length=100,  # CharField で max_length が有効です
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': '検索したいキーワードを入力'})  # widgetのインデントを修正
+    )
+
+# 商品フォーム
+class ProductForm(forms.ModelForm):
+    class Meta:  # Mera → Meta に修正
+        model = Product
+        fields = ['name', 'description', 'price', 'category']
